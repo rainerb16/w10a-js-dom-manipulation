6ae467edc7e30650876b4a6fb3c110d2b266b2eb
Rainer Brieger <hello@rainerb.com>
2020-08-25 20:19:35 -0600

    Finished all 9 example requirements for assignment

--- js/app.js
+++ js/app.js
@@ -2,6 +2,7 @@
 var headerText = document.querySelector("body h1");
 headerText.style.color = "maroon";
 headerText.style.textDecoration = "underline";
+headerText.style.textAlign = "center";
 
 // 2 - querySelectorAll()
 var pTags = document.querySelectorAll("p");
@@ -20,3 +21,26 @@ for(var i = 0; i < containerThree.length; i++) {
     containerThree[i].innerText += " I'M HAVING A BLAST IN THIS CLASS!";
     containerThree[i].style.color = "blue";
 }
+
+// 5 - getElementsByTagName
+var tagName = document.getElementsByTagName("h3")
+for(var i = 0; i < tagName.length; i++) {
+    tagName[i].style.border = "5px solid green";
+}
+
+// 6 - createElement()
+var newElement = document.createElement("h3");
+var parentElement = document.getElementById("parent");
+parentElement.append(newElement);
+
+// 7 - createTextNode()
+var createText = document.createTextNode("DOM DOM DOM DOM");
+newElement.append(createText);
+
+// 8 - removeChild()
+var remove = document.getElementById("parent");
+remove.removeChild(remove.childNodes[1]);
+
+// 9 - classList add(), remove(), toggle()
+var addElement = document.getElementById("image-dom");
+addElement.classList.add("image-dom-tree");
